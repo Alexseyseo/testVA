1cc8a87469edf15698f01239040c9aeb93dfd718
Alexseyseo <Alexsey.seo@gmail.com>
2018-12-17 16:32:48 +0300

    Доработка автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@ var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var rename = require("gulp-rename");
 var del = require("del");
-
 var sass = require("gulp-sass");
 var csso = require("gulp-csso");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
-
 var server = require("browser-sync").create();
 
 gulp.task("clean", function () {
@@ -20,8 +17,7 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
       "src/fonts/**/*.{woff2,woff,otf}",
-      "src/img/**",
-      "src/js/**"
+      "src/img/**"
     ], {
       base: "src"
     })
@@ -40,21 +36,49 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"));
 });
 
+gulp.task("bootstrap", function () {
+  return gulp.src("node_modules/bootstrap/scss/bootstrap-grid.scss")
+    .pipe(sass())
+    .pipe(csso())
+    .pipe(rename("bootstrap-grid.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("slick", function () {
+  return gulp.src("node_modules/slick-carousel/slick/slick.css")
+    .pipe(csso())
+    .pipe(rename("slick.min.css"))
+    .pipe(gulp.dest("build/slick"))
+    .pipe(gulp.src("node_modules/slick-carousel/slick/slick-theme.css"))
+    .pipe(csso())
+    .pipe(rename("slick-theme.min.css"))
+    .pipe(gulp.dest("build/slick"))
+    .pipe(gulp.src([
+      "node_modules/slick-carousel/slick/slick.min.js",
+      "node_modules/slick-carousel/slick/fonts/**",
+      "node_modules/slick-carousel/slick/ajax-loader.gif"
+    ],  {
+      base: "node_modules/slick-carousel/slick/"
+    }))
+    .pipe(gulp.dest("build/slick"));
+});
+
 gulp.task("js", function () {
-  return gulp.src("src/js/*.js")
+  return gulp.src(["src/js/*.js", "node_modules/jquery/dist/jquery.min.js"])
     .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("images", function () {
   return gulp.src("src/img/**/*.png")
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3})
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
     ]))
     .pipe(gulp.dest("src/img"));
 });
@@ -74,6 +98,9 @@ gulp.task("build", gulp.series(
   "clean",
   "copy",
   "css",
+  "js",
+  "slick",
+  "bootstrap",
   "html"
 ));
 
